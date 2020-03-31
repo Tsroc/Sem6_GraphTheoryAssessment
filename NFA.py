b3d2166b8a813f5b34813f7b987395b7ecab362b
Eoin Wilkie <G00363134@gmit.ie>
2020-03-31 19:01:45 +0100

    Fixed State

--- NFA.py
+++ NFA.py
@@ -7,7 +7,7 @@ class State:
 	# Constructor.
 	def __init__(self, label=None, edges=[]):
 		# Every state has 0, 1, or 2 edges from it.
-		self.edges = edges
+		self.edges = edges if edges else []
 		# Label for the arows. Nne means epsilon.
 		self.label = label
 	
